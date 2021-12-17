45e7a7a9134ee30038a33105e0b624b9c9326575
Dmytro Moshunov <dima.moshunov.2002@gmail.com>
2021-12-17 14:49:29 +0200

    fixed some issues

--- app/src/main/java/com/example/trempelapp/data_layer/network/UserService.kt
+++ app/src/main/java/com/example/trempelapp/data_layer/network/UserService.kt
@@ -1,7 +1,9 @@
 package com.example.trempelapp.data_layer.network
 
 import io.reactivex.Single
-import retrofit2.http.*
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
 
 interface UserService {
 
